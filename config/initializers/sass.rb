a1c9b73b2cb56a6509b803cba8caaee3fe849d4c
Steven Jenkins <steven.jenkins@gmail.com>
2011-12-22 20:12:24 -0500

    have sass write to tmp, for h eroku

---
+++ config/initializers/sass.rb
@@ -0,0 +1,13 @@
+Sass::Plugin.options.merge! (
+    :template_location => 'public/stylesheets/sass',
+    :css_location => 'tmp/stylesheets'
+    )
+
+Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+Rails.configuration.middleware.insert_before('Sass::Sendfile', 'Sass::Plugin::Rack')
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => "#{Rails.root}/tmp")
+
+
